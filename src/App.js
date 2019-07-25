0bc5de3258e12482bc97d639bb43398500a94cfe
wognsdl1126 <wognsdl1126@gmail.com>
2019-07-25 21:45:08 +0900

    20190725

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 
@@ -50,7 +49,8 @@ class App extends React.Component {
     console.log("phone:",this.state.phone);    
     // const newContact = this.state.contact.slice(0);
     // newContact.push()
-    const lastId = this.state.contact[this.state.contact.length - 1].id + 1;
+    const length = this.state.contact.length;
+    const lastId =  length > 0 ? this.state.contact[this.state.contact.length - 1].id +1 : 1;
     this.setState({
       contact:[
         ...this.state.contact,
@@ -68,6 +68,22 @@ class App extends React.Component {
   _handleChangeState = (target,value) => {
     this.setState({[target] : value})
   }
+  /**
+   * 수정모드
+   */
+  _handleToggleEditMode = (id) =>{
+    this.setState(prevState => ({isEditMode: prevState.isEditMode === id ? -1 : id}))      
+  }
+
+  
+  /**
+   * 수정값반영
+   */
+  _handleChangeContact = (index, event) => {
+    const newContact = this.state.contact.slice(0);
+    newContact[index][event.target.name] = event.target.value;
+    this.setState({contact : newContact});
+  }
   // _handleChangeState = event => {
   //   console.log("E :", event);
   //   this.setState({[event.target.name] : event.target.value})
@@ -78,6 +94,11 @@ class App extends React.Component {
    */
   render(){
     const {contact, name, phone} = this.state;
+    let data = contact
+    let isEditMode = this.state.isEditMode
+    if(name){
+      data = contact.filter(c=> c.name.indexOf(name) > -1)
+    }
     return (
       <div className="App">
         <div>
@@ -86,6 +107,7 @@ class App extends React.Component {
               <tr>
                 <th>name</th>
                 <th>phone</th>
+                <th>-</th>
                 <th>Delete</th>
               </tr>
             </thead>
@@ -93,20 +115,32 @@ class App extends React.Component {
               <tr>
                 <td><input value = {name} onChange={(event) => this._handleChangeState('name',event.target.value)} placeholder="name"/></td>
                 <td><input value = {phone} onChange={(event) => this._handleChangeState('phone',event.target.value)} placeholder="phone"/></td>
-                <td>
+                <td colSpan="2">
                   <button onClick={()=> this._handleChange()}>new</button>
                 </td>
               </tr>
+                            
               {
-                contact.map(Item => (
-                <tr>
-                <td>{Item.name}</td>
-                <td>{Item.phone}</td>
-                <td>
-                  <button onClick ={()=> this._handleRemoveContact(Item.id) }>Delete</button>
-                </td>
+                data.map((Item, index) => (
+                <tr key ={index.toString()}>
+                  {(isEditMode > -1 && isEditMode === Item.id) ? (
+                    <>
+                    <td><input name="name" value={Item.name} onChange={(event) => this._handleChangeContact(index,event)}/></td>
+                    <td><input name ="phone" value={Item.phone} onChange={(event) => this._handleChangeContact(index,event)} /></td>         
+                    </>
+                   ) : (
+                    <>
+                    <td>{Item.name}</td>
+                    <td>{Item.phone}</td>
+                    </>
+                  )}
+                  <td>
+                    <button onClick ={()=> this._handleToggleEditMode(Item.id) }>Edit</button>
+                  </td>
+                  <td>
+                    <button onClick ={()=> this._handleRemoveContact(Item.id) }>Delete</button>
+                  </td>
                 </tr>
-
                 ))}                            
             </tbody>
           </table>
